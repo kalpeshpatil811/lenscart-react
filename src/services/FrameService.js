a7d17c3879a754a27dd184c01cb1431a2373bb00
laxman-adkune <adkunelaxman1223@gmail.com>
2022-08-13 14:09:20 +0530

    frame service

---
+++ src/services/FrameService.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+const FRAME_API_BASE_URL = "http://localhost:9091/lenscart/frame";
+class FrameService{
+    
+	getFrames() {
+		return axios.get(FRAME_API_BASE_URL);
+	}
+
+	createFrame(frame) {
+		return axios.post(FRAME_API_BASE_URL, frame);
+	}
+
+	updateFrame(frame) {
+		return axios.put(FRAME_API_BASE_URL, frame);
+	}
+
+	getFrameById(frameId) {
+		return axios.get(FRAME_API_BASE_URL + "/" + frameId);
+	}
+
+	deleteFrame(frameId) {
+		return axios.delete(FRAME_API_BASE_URL + "/" + frameId);
+	}
+}
+
+export default new FrameService();
