84d4252dc4f057920ed855cbdc3ce27db15dd170
Sachin Phatak <phatak.sachin12@gmail.com>
2022-08-14 00:58:42 +0530

    added router and addlens

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import AddSunGlass from "./components/SunGlass/AddSunGlass";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddLens from "./components/Lens/AddLens";
 import ShowAllSunGlasses from "./components/SunGlass/ShowAllSunGlasses";
 
@@ -8,11 +8,16 @@ import ShowAllLenses from "./components/Lens/ShowAllLenses"
 function App() {
 	return (
 		<div className="App">
-			<h1 align="center">LensCart React App</h1>
-			{/* <AddSunGlass /> */}
-			<ShowAllSunGlasses />
-			{/* <AddLens /> */}
-			{/* <ShowAllLenses /> */}
+		<Router>
+			{/* <h1 align="center">LensCart React App</h1> */}
+			{/* <ShowAllLenses />  */}
+				<Routes>
+					<Route path="/addsunglass" element={<AddSunGlass />} />
+					<Route path="/showallsunglasses" element={<ShowAllSunGlasses />} />
+					<Route path="/addlens" element={<AddLens />} />
+					<Route path="/showalllenses" element={<ShowAllLenses /> } />
+				</Routes>
+			</Router>
 		</div>
 	);
 }
--- src/components/Lens/AddLens.jsx
+++ src/components/Lens/AddLens.jsx
@@ -1,58 +1,133 @@
 import React, { useState } from "react";
+import LensService from "../../services/LensService";
+import { useNavigate } from "react-router-dom";
 
 const AddLens = () => {
-	// const[brand,setBrand] = useState('')
-	// const[image,setImage] = useState('')
-	// const[shape,setShape] = useState('')
-	// const[price,setPrice] = useState('')
-	// const[color,setColor] = useState('')
+	// Create state variables for each input field
+	// const [sunGlassName, setSunGlassName] = useState("");
+	const [brand, setBrand] = useState("");
+	const [price, setPrice] = useState("");
+	const [color, setColor] = useState("");
+	const [shape, setShape] = useState("");
+	const [lenseImage, setlenseImage] = useState("");
+	// const [glassColor, setGlassColor] = useState("");
+	// const [weight, setWeight] = useState("");
+	const navigate = useNavigate();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const newLens = {
+			// sunGlassName: sunGlassName,
+			brand: brand,
+			price: price,
+			color: color,
+			shape: shape,
+			lenseImage: lenseImage
+		};
+		console.log(newLens);
+		LensService.createLens(newLens)
+			.then((res) => {
+				console.log(res);
+				console.log("Lens added successfully");
+				alert("Lens added successfully");
+			})
+			.catch((err) => {
+				console.log(err);
+				console.log("Error adding Lens");
+			})
+			.finally(() => {
+				navigate("/showalllenses");
+			});
+	};
+
 	return (
 		<div align="center">
 			<div className="card" style={{ width: "50rem" }}>
-				<form className="card-body">
-					<h5 class="card-title">Add Lens</h5>
-					{/* <!-- 2 column grid layout with text inputs for the first and last names --> */}
-					<div className="row mb-4">
-						<div className="col">
-							<div className="form-outline">
-								<input type="text" id="form6Example2" className="form-control" />
-								<label className="form-label" for="form6Example2">
-									Brand
-								</label>
-							</div>
-						</div>
-					</div>
-
-					{/* <!-- Text input --> */}
-					<div className="form-outline mb-4">
-						<input type="text" id="form6Example3" className="form-control" />
-						<label className="form-label" for="form6Example3">
-							Price	
+				<form className="card-body" onSubmit={(e) => handleSubmit(e)}>
+					<h5 className="card-title">Add Contact Lens</h5>
+					<hr />
+					{/* <!-- SunGlass Name input --> */}
+					{/* <div className="form-outline mb-4">
+						<label className="form-label" htmlFor="sunGlassName">
+							SunGlass Name
 						</label>
-					</div>
+						<input
+							type="text"
+							id="sunGlassName"
+							className="form-control"
+							value={sunGlassName}
+							onChange={(e) => setSunGlassName(e.target.value)}
+						/>
+					</div> */}
 
-					{/* <!-- Text input --> */}
+					{/* <!-- Brand input --> */}
 					<div className="form-outline mb-4">
-						<input type="number" id="form6Example4" className="form-control" />
-						<label className="form-label" for="form6Example4">
-							Image
+						<label className="form-label" htmlFor="brand">
+							Brand
 						</label>
+						<input
+							type="text"
+							id="brand"
+							className="form-control"
+							value={brand}
+							onChange={(e) => setBrand(e.target.value)}
+						/>
 					</div>
 
-					{/* <!-- Email input --> */}
+					{/* <!-- Price input --> */}
 					<div className="form-outline mb-4">
-						<input type="text" id="form6Example5" className="form-control" />
-						<label className="form-label" for="form6Example5">
-							Shape
+						<label className="form-label" htmlFor="price">
+							Price
 						</label>
+						<input
+							type="number"
+							id="price"
+							className="form-control"
+							value={price}
+							onChange={(e) => setPrice(e.target.value)}
+						/>
 					</div>
 
-					{/* <!-- Number input --> */}
+					{/* <!-- Color input --> */}
 					<div className="form-outline mb-4">
-						<input type="text" id="form6Example6" className="form-control" />
-						<label className="form-label" for="form6Example6">
+						<label className="form-label" htmlFor="color">
 							Color
 						</label>
+						<input
+							type="text"
+							id="frameColor"
+							className="form-control"
+							value={color}
+							onChange={(e) => setColor(e.target.value)}
+						/>
+					</div>
+
+					{/* <!-- Shape input --> */}
+					<div className="form-outline mb-4">
+						<label className="form-label" htmlFor="shape">
+							Shape
+						</label>
+						<input
+							type="text"
+							id="shape"
+							className="form-control"
+							value={shape}
+							onChange={(e) => setShape(e.target.value)}
+						/>
+					</div>
+
+					{/* <!-- Image URL input --> */}
+					<div className="form-outline mb-4">
+						<label className="form-label" htmlFor="lenseImage">
+							Image URL
+						</label>
+						<input
+							type="text"
+							id="image"
+							className="form-control"
+							value={lenseImage}
+							onChange={(e) => setlenseImage(e.target.value)}
+						/>
 					</div>
 
 					{/* <!-- Submit button --> */}
