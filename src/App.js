043dd3d6da5772a6501826743b1386856292f485
akshatagurupwar <98731533+akshatagurupwar@users.noreply.github.com>
2022-08-13 18:13:30 +0530

    Login And Register

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import AddCustomer from "./components/Customer/AddCustomer";
+import Login from "./components/Customer/Login";
+import ShowAllSunGlasses from "./components/SunGlass/ShowAllSunGlasses";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 function App() {
 	return (
 		<div className="App">
 			<h1 align="center">LensCart React App</h1>
+			<Router>
+				<Routes>
+
+					<Route path="/addcustomer" element={<AddCustomer />} />
+					<Route path="/login" element={<Login />} />
+
+				</Routes>
+			</Router>
 		</div>
 	);
 }
