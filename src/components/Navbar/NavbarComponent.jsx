baa1c4843411be1ce0781ccecb7d3276660adb56
Sachin Phatak <phatak.sachin12@gmail.com>
2022-08-16 23:42:54 +0530

    added login navbar hide

--- src/components/Navbar/NavbarComponent.jsx
+++ src/components/Navbar/NavbarComponent.jsx
@@ -2,11 +2,15 @@ import Container from "react-bootstrap/Container";
 import { Nav, Navbar, NavDropdown, Button } from "react-bootstrap";
 import { Cart, PersonFill } from "react-bootstrap-icons";
 import { useNavigate } from "react-router-dom";
-import { removeCustomerInfo } from "../Customer/CustomerInfo";
+import { getCustomerInfo, removeCustomerInfo } from "../Customer/CustomerInfo";
 
 function NavbarComponent() {
 	const navigate = useNavigate();
 
+    let isLoggedIn=false;
+	if(getCustomerInfo()!=null){
+		isLoggedIn=true;
+	}
 
 	const handleLogout = () => {
 
@@ -22,6 +26,7 @@ function NavbarComponent() {
 			<Container>
 				<Navbar.Brand onClick={() => navigate("/")}>LensCart</Navbar.Brand>
 				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				{isLoggedIn &&
 				<Navbar.Collapse id="responsive-navbar-nav">
 					<Nav className="me-auto">
 						<Nav.Link onClick={() => navigate("/showallframes")}>Frames</Nav.Link>
@@ -45,6 +50,7 @@ function NavbarComponent() {
 						</NavDropdown>
 					</Nav>
 				</Navbar.Collapse>
+				}
 			</Container>
 		</Navbar>
 	);
